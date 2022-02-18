f129cfe462e6330d49487ed108c07e9e8316c147
alin <823301694@qq.com>
2022-02-18 15:26:06 +0800

    FindCenterOfStarGraph

---
+++ src/main/java/top/alin/solution/FindCenterOfStarGraph.java
@@ -0,0 +1,12 @@
+package top.alin.solution;
+
+public class FindCenterOfStarGraph {
+    public int findCenter(int[][] edges) {
+        int a = edges[0][0], b = edges[0][1];
+        if(a == edges[1][0] || a == edges[1][1]) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+}
