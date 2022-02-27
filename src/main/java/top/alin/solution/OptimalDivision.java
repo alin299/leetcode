b3d624c25549ba070ab8b49e8abc0c4e68132ca7
alin <823301694@qq.com>
2022-02-27 12:43:44 +0800

    OptimalDivision

---
+++ src/main/java/top/alin/solution/OptimalDivision.java
@@ -0,0 +1,17 @@
+package top.alin.solution;
+
+public class OptimalDivision {
+    public String optimalDivision(int[] nums) {
+        int n = nums.length;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(nums[i]);
+            if (i + 1 < n) sb.append("/");
+        }
+        if (n > 2) {
+            sb.insert(sb.indexOf("/") + 1, "(");
+            sb.append(")");
+        }
+        return sb.toString();
+    }
+}
