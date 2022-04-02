29fb9eabcd8696a8cfc890433fd11f3905939c4c
alin <823301694@qq.com>
2022-04-02 10:57:22 +0800

    TwoSum

---
+++ src/main/java/top/alin/solution/TwoSum.java
@@ -0,0 +1,17 @@
+package top.alin.solution;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
