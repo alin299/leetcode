43ec7f082189b5a07bf9f46b43d1cdb9d5768e06
alin <823301694@qq.com>
2022-04-17 18:02:29 +0800

    SingleNumber

---
+++ src/main/java/top/alin/solution/SingleNumber.java
@@ -0,0 +1,11 @@
+package top.alin.solution;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
+}
