3a637984a3dfcfcd6df70c690aa6619d9b186623
alin <823301694@qq.com>
2022-05-27 10:25:11 +0800

    MinimumConsecutiveCardsToPickUp

---
+++ src/main/java/top/alin/solution/MinimumConsecutiveCardsToPickUp.java
@@ -0,0 +1,17 @@
+package top.alin.solution;
+
+import java.util.HashMap;
+
+public class MinimumConsecutiveCardsToPickUp {
+    public int minimumCardPickup(int[] cards) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0; i < cards.length; i++) {
+            if (map.containsKey(cards[i])) {
+                ans = Math.min(ans, i - map.get(cards[i]) + 1);
+            }
+            map.put(cards[i], i);
+        }
+        return ans > cards.length ? -1 : ans;
+    }
+}
