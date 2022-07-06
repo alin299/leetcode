c279f0995a5369bea9d68d1a45733d2296ca08ca
alin <823301694@qq.com>
2022-07-06 15:48:08 +0800

    LongestCommonPrefix

---
+++ src/main/java/top/alin/solution/LongestCommonPrefix.java
@@ -0,0 +1,19 @@
+package top.alin.solution;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int len = strs[0].length();
+        for(int i = 0; i < len; i++) {
+            char c = strs[0].charAt(i);
+            for(int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
