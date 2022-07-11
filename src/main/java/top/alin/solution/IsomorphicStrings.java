525758037487e25ffb446d69baf38238b7f0d26f
alin <823301694@qq.com>
2022-07-11 11:55:04 +0800

    IsomorphicStrings

---
+++ src/main/java/top/alin/solution/IsomorphicStrings.java
@@ -0,0 +1,26 @@
+package top.alin.solution;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                if (!map.get(s.charAt(i)).equals(t.charAt(i))) {
+                    return false;
+                }
+            } else {
+                if (!set.add(t.charAt(i))) {
+                    return false;
+                }
+                map.put(s.charAt(i), t.charAt(i));
+            }
+        }
+        return true;
+    }
+}
