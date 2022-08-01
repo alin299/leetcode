8d13049326f9067778a2a9ba356f5303543aad27
alin <823301694@qq.com>
2022-08-01 10:41:52 +0800

    PathSum

---
+++ src/main/java/top/alin/solution/PathSum.java
@@ -0,0 +1,15 @@
+package top.alin.solution;
+
+import top.alin.common.TreeNode;
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
