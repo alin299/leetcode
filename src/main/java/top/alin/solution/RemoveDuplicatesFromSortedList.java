db82a68c00660cb338ad0e0d934ba5a8c388b271
alin <823301694@qq.com>
2022-09-02 19:57:43 +0800

    RemoveDuplicatesFromSortedList

---
+++ src/main/java/top/alin/solution/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,16 @@
+package top.alin.solution;
+
+import top.alin.common.ListNode;
+
+public class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        head.next = deleteDuplicates(head.next);
+        if (head.val == head.next.val) {
+            head = head.next;
+        }
+        return head;
+    }
+}
