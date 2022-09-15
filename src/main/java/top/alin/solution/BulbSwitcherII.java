b1cf0144791f372da74b9e45effd003b8919d418
alin <823301694@qq.com>
2022-09-15 14:16:08 +0800

    BulbSwitcherII

---
+++ src/main/java/top/alin/solution/BulbSwitcherII.java
@@ -0,0 +1,16 @@
+package top.alin.solution;
+
+public class BulbSwitcherII {
+    public int flipLights(int n, int presses) {
+        if (presses == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 2;
+        } else if (n == 2) {
+            return presses == 1 ? 3 : 4;
+        } else {
+            return presses == 1 ? 4 : presses == 2 ? 7 : 8;
+        }
+    }
+}
